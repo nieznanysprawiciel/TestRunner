09e178280602d858f884113d20fe8e24fed5ba57
nieznanysprawiciel <nieznany.sprawiciel@gmail.com>
2019-06-13 22:22:44 +0200

    Arguments parsing function

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,25 @@
 mod execution;
 
 
+extern crate clap;
 
+
+use clap::{ Arg, App };
 use std::path::PathBuf;
 
 
 
+struct Config
+{
+	pub discovery_dir: PathBuf
+}
+
+
 fn main()
 {
+	let config = parse_cmd_args();
+
+
 	let test_path = PathBuf::from( "C:\\dupa\\dupa" );
 	let working_dir = PathBuf::from( "C:\\dupa\\dupa\\work" );
 
@@ -19,4 +31,21 @@ fn main()
 	let runner = execution::Runner::new( to_execute );
 
 	runner.run();
+}
+
+fn parse_cmd_args() -> Config
+{
+    let matches = App::new( "Test Runner" )
+        .version( "0.1.0" )
+        .author( "nieznanysprawiciel <nieznany.sprawiciel@gmail.com>" )
+        .about( "Application for discovering and running tests in visual studio projects without using build in testing tools." )
+        .arg( Arg::with_name( "dir" )
+            .short( "d" )
+            .long( "discovery dir" )
+            .help( "Directory to look for tests." ))
+        .get_matches();
+
+	let discovery_dir = PathBuf::from( matches.value_of( "d" ).unwrap_or( "" ) );
+
+	Config{ discovery_dir }
 }
