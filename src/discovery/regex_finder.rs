0e2db54374879df6bd529ae5ddfb199a6d1b4f39
nieznanysprawiciel <nieznany.sprawiciel@gmail.com>
2019-06-30 13:52:23 +0200

    RegexFinder top down implementation - part 1

--- src/discovery/regex_finder.rs
+++ src/discovery/regex_finder.rs
@@ -1,9 +1,16 @@
+extern crate walkdir;
+extern crate regex;
+
 
 use std::path::PathBuf;
+use std::path::Path;
 
 use super::finder::Finder;
 use crate::test_info::TestDescription;
 use crate::test_info::TestsConfig;
+
+use regex::Regex;
+use walkdir::WalkDir;
 
 
 
@@ -17,16 +24,61 @@ pub struct RegexFinder
 impl Finder for RegexFinder
 {
 
+	// ================================ //
+	//
 	fn			discover			( &self, root_path: PathBuf, config: &TestsConfig ) -> Vec< TestDescription >
 	{
-		Vec::new()
+		let mut tests = Vec::new();
+
+		 for entry in WalkDir::new( root_path )
+				.follow_links( true )
+				.into_iter()
+				.filter_entry( |entry| entry.file_type().is_file() )
+		{
+			let filepath = entry.unwrap().into_path();
+
+			if self.is_test( &filepath, &config )
+			{
+				let test_description = TestDescription{ working_dir: config.working_dir.clone(), test_path: filepath };
+				tests.push( test_description );
+			}
+		}
+
+		return tests
 	}
 }
 
 impl RegexFinder
 {
+	// ================================ //
+	//
 	pub fn		new					() -> RegexFinder
 	{
 		RegexFinder{}
 	}
+
+	// ================================ //
+	//
+	fn			is_test				( &self, path: &Path, config: &TestsConfig ) -> bool
+	{
+		let filename = path.file_name().unwrap().to_str().unwrap().to_lowercase();
+		let test_regex = Regex::new( r"test.*$" ).unwrap();
+
+		test_regex.is_match( &filename )
+	}
+
 }
+
+
+// ================================ //
+// Tests
+
+#[test]
+fn			test_is_test		()
+{
+	let tests_config = TestsConfig{ working_dir: PathBuf::from( "" ), ..Default::default() };
+	let finder = RegexFinder::new();
+
+	assert!( finder.is_test( &PathBuf::from( "working-dir/test-structure1/test_important_things.exe" ), &tests_config ) )
+}
+
