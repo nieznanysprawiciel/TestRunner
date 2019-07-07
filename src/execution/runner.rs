394749786aa12c5e498392e8768d932f28a8cf52
nieznanysprawiciel <nieznany.sprawiciel@gmail.com>
2019-07-07 14:21:06 +0200

    Running tests

--- src/execution/runner.rs
+++ src/execution/runner.rs
@@ -1,6 +1,10 @@
 
 use crate::test_info::TestDescription;
 
+use std::process::{ Command, Stdio };
+use std::error::Error;
+use std::io::prelude::*;
+
 
 pub struct Runner
 {
@@ -25,6 +29,25 @@ impl Runner
 		for test in &self.tests
 		{
 			println!( "{}", test.test_path.display() );
+
+			let process = match Command::new( &test.test_path )
+											.current_dir( &test.working_dir )
+											.stdin( Stdio::piped() )
+											.stdout( Stdio::piped() )
+											.spawn()
+			{
+				Err( why ) => panic!( "couldn't spawn test: {}", why.description() ),
+				Ok( process ) => process,
+			};
+
+			let mut test_output = String::new();
+			match process.stdout.unwrap().read_to_string( &mut test_output )
+			{
+				Err( why ) => panic!( "couldn't read wc stdout: {}", why.description() ),
+				Ok( _ ) => print!( "wc responded with:\n{}", test_output ),
+			}
+
+			println!( "{}", test_output );
 		}
 	}
 }
